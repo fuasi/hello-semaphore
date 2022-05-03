2c4d2e332f2ac0c744a8c6f9786023e963c3076c
Kovit <1120236287@qq.com>
2022-05-03 19:40:27 +0800

    INIT

---
+++ app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.GET("/hello", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"code": 20000,
+			"msg":  "hello",
+		})
+	})
+	err := engine.Run(":7666")
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+}
