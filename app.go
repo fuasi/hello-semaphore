ec587c4d9914b7fce222753f81355aa4f81c5586
Kovit <1120236287@qq.com>
2022-05-04 18:26:46 +0800

    INIT

--- app.go
+++ app.go
@@ -1,35 +1,14 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-	"golang.org/x/crypto/ssh"
-	"log"
-)
-
 func main() {
-	client, err := ssh.Dial("tcp", "lanks.top:22", &ssh.ClientConfig{
-		User:            "root",
-		Auth:            []ssh.AuthMethod{ssh.Password("")},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+
+	app := gin.Default()
+	app.GET("/", func(context *gin.Context) {
+		context.JSON(200, struct {
+			Msg  string
+			Code int
+		}{"LSCB", 200})
 	})
-	if err != nil {
-		log.Fatalf("SSH dial error: %s", err.Error())
-	}
 
-	// 建立新会话
-	session, err := client.NewSession()
-	if err != nil {
-		log.Fatalf("new session error: %s", err.Error())
-	}
-
-	defer session.Close()
-
-	var b bytes.Buffer
-	session.Stdout = &b
-	err = session.Run("ls")
-	if err != nil {
-		panic("Failed to run: " + err.Error())
-	}
-	fmt.Println(b.String())
+	app.Run(":7666")
 }
